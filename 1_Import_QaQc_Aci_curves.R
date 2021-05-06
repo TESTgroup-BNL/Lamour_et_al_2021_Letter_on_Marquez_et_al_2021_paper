72b526ac6794b0fd7b900f89d3919c60410c4fe1
JulienLamour <jlamour@bnl.gov>
2021-05-06 17:56:48 -0400

    Creation of Fig 1 and 2

--- 1_Import_QaQc_Aci_curves.R
+++ 1_Import_QaQc_Aci_curves.R
@@ -1,72 +1,44 @@
-
-
 library(LeafGasExchange)
 source('0_NewTheoryGasex.R')
-setwd("~/GitHub/NewTheoryGasEx")
+setwd("~/GitHub/Marquez_et_al_2021_New_Gasex_theory")
 
-ls.files=dir('~/GitHub/NewTheoryGasEx/Datasets/ACi_Panama_2020/',recursive = TRUE,full.names = TRUE)
 
-remove_Aci_dupplicate<-function(Aci_curve){
-  Aci_curve=round(Aci_curve,-1)
-  remove=rep('NO',length(Aci_curve))
-  remove[length(Aci_curve)]='YES'
-  remove[which(duplicated(Aci_curve[1:(length(Aci_curve)-1)],fromLast = TRUE))]='YES'
-  remove[1:5]='NO'
-  remove[which(duplicated(Aci_curve[1:5],fromLast = TRUE))]='YES'
-  return(remove)
+Aci_data_Panama=f.import_licor6800(file = '~/GitHub/Marquez_et_al_2021_New_Gasex_theory/Datasets/20200122_ACi_Lindsey_BNL17167.xlsx')
+colnames(Aci_data_Panama)[colnames(Aci_data_Panama)=='X.U.0394.Pcham']='X.Pcham'
+
+plot(x=Aci_data_Panama$Ci,Aci_data_Panama$A,cex=2)
+text(x=Aci_data_Panama$Ci,Aci_data_Panama$A, labels=Aci_data_Panama$obs,cex=0.7)
+remove=c(19,9:11)
+Aci_data_Panama=Aci_data_Panama[!Aci_data_Panama$obs%in%remove,]
+points(x=Aci_data_Panama$Ci,y=Aci_data_Panama$A,cex=2,col='red')
+'Guatteria dumetorum'
+#####################
+#### Arctic data  ###
+#####################
+Aci_data_arctic=read.csv('Datasets/NGEE-Arctic_A-Ci_curves_2012-2015.csv',skip = 8,na.strings = '-9999',header = TRUE)
+Aci_data_arctic=Aci_data_arctic[Aci_data_arctic$Sample_Barcode=='1557',]
+Aci_data_arctic$obs=1:18
+plot(x=Aci_data_arctic$Ci,Aci_data_arctic$Photo,cex=2)
+text(x=Aci_data_arctic$Ci,Aci_data_arctic$Photo, labels=Aci_data_arctic$obs,cex=0.7)
+remove=c(7,8:10,18)
+Aci_data_arctic=Aci_data_arctic[!Aci_data_arctic$obs%in%remove,]
+points(x=Aci_data_arctic$Ci,y=Aci_data_arctic$Photo,cex=2,col='red')
+'Petasites frigidus'
+
+################
+### Oak data ###
+################
+Aci_data_oak=read.csv('Datasets/raw_gas_exchange_li6400_oak.csv',header = TRUE)
+for (i in 1:length(unique(Aci_data_oak$Sample_ID))){
+plot(x=Aci_data_oak[Aci_data_oak$Sample_ID==unique(Aci_data_oak$Sample_ID)[i],'Ci'],
+     y=Aci_data_oak[Aci_data_oak$Sample_ID==unique(Aci_data_oak$Sample_ID)[i],'Photo'],cex=2, main= i)
+text(x=Aci_data_oak[Aci_data_oak$Sample_ID==unique(Aci_data_oak$Sample_ID)[i],'Ci'],
+     y=Aci_data_oak[Aci_data_oak$Sample_ID==unique(Aci_data_oak$Sample_ID)[i],'Photo'],
+     labels = Aci_data_oak[Aci_data_oak$Sample_ID==unique(Aci_data_oak$Sample_ID)[i],'Obs'],cex=0.7)  
 }
+unique(Aci_data_oak$Sample_ID)[14]
+Aci_data_oak=Aci_data_oak[Aci_data_oak$Sample_ID=='15072',]
 
-Aci_data=do.call("rbind", apply(X = as.matrix(ls.files),FUN = f.import_licor6800,MARGIN = 1))
-colnames(Aci_data)['X.U.0394.Pcham']='X.Pcham'
 
-## A binary column is added to the file to detect the points that should be removed for the fitting procedures
-Aci_data$remove='NO'
-## All the Aci_data with Ci<0 or Ci>2000 are considered wrong
-Aci_data[Aci_data$Ci<0|Aci_data$Ci>2000,'remove']='YES'
 
-## The duplicated points are removed using this function
-remove=ave(Aci_data$CO2_r,Aci_data$file,FUN=remove_Aci_dupplicate)
-Aci_data[which(remove=='YES'),'remove']='YES'
-
-data$remove=factor(data$remove)
-pdf(file='QA_QC_Aci_Panama_2020.pdf',)
-par(mar = c(5,5,2,5))
-by(data = Aci_data,INDICES = Aci_data$Barcode,FUN = function(x){
-  
-  mean_temp=mean(x$Tleaf)
-  max_A=max(x$A)  
-  b=28
-  a=(32-28)/max_A
-  
-  #(ggplot(data=x,aes(x=Ci,y=A,color=remove,label=Obs))+ggtitle(unique(x$Leaf_Barcode))
-  # +geom_point(size=4,alpha=0.5)+geom_text(aes(label=Obs),col='black')
-  # +geom_line(data=x,aes(x=Ci,y=(Tleaf-b)/a),type='dotted',color='grey')+theme_bw() 
-  # +scale_y_continuous(sec.axis = sec_axis(~ . *a+b, name = "Leaf Temp")
-  # ))
-  max_ci=max(x$Ci)
-  if(max(x$Ci)>2000){max_ci=1800}
-  plot(x=x$Ci,y=x$A,main=unique(x$Barcode),xlab="Ci",ylab='A',cex=2,xlim=c(0,max_ci))
-  points(x=x[x$remove=='NO','Ci'],y=x[x$remove=='NO','A'],col='red',cex=2)
-  text(x=x$Ci,y=x$A, labels=x$obs,cex=0.7)
-  try(text(x=x[x$remove=='NO','Ci'],y=x[x$remove=='NO','A'],labels=x[x$remove=='NO','Obs'],col='red',cex=0.7))
-  par(new=TRUE)
-  plot(x=x$Ci,y=x$Tleaf,col='grey',axes=F, xlab=NA, ylab=NA,ylim=c(27,32),xlim=c(0,max_ci))
-  axis(side = 4)
-  mtext(side = 4, line = 3, 'Tleaf')
-})
-dev.off()
-
-ls_remove=c('BNL17516','BNL17565','BNL16587','BNL17659','BNL17662','BNL18443')
-Aci_data=Aci_data[!Aci_data$Barcode%in%ls_remove,]
-Aci_data[Aci_data$Barcode=='BNL17195'&Aci_data$obs%in%c(13),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17197'&Aci_data$obs%in%c(2,5),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17521'&Aci_data$obs%in%c(18),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17534'&Aci_data$obs%in%c(6),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17540'&Aci_data$obs%in%c(6),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17639'&Aci_data$obs%in%c(6),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17542'&Aci_data$obs%in%c(5),'Remove']='YES'
-Aci_data[Aci_data$Barcode=='BNL17741'&Aci_data$obs%in%c(6),'Remove']='YES'
-
-Aci_data=Aci_data[Aci_data$remove=='NO',]
-
-save(Aci_data,file='1_Aci_data_QaQc.Rdata')
+save(Aci_data_oak,Aci_data_arctic,Aci_data_Panama,file='1_Aci_data_QaQc.Rdata')
---
+++ 2_Recalculation_ACi_MSWF_model.R
@@ -0,0 +1,328 @@
+## The aim of this code is to test the effect of the new theory on the ACi measurement
+## and fitting
+
+setwd("~/GitHub/Marquez_et_al_2021_New_Gasex_theory")
+library(LeafGasExchange)
+library(cowplot)
+source('0_NewTheoryGasex.R')
+load('1_Aci_data_QaQc.Rdata')
+
+
+#############################################################
+#### Recalculation of the ACi values from the Panama data ###
+#############################################################
+
+colnames(Aci_data_Panama)[colnames(Aci_data_Panama)=='X.U.0394.Pcham']='X.Pcham'
+
+Aci=Aci_data_Panama[order(Aci_data_Panama$Ci),]
+Recomp_Aci_Panama=Aci[,c('A','Tleaf','Ci','Qin','gsw')]
+Recomp_Aci_Panama$Recomp='Reference'
+
+## I recompute this file using Marquez et al. 2021 theory using different values 
+## of cuticular conductance
+for(gcw in seq(0,25*10^-3,1*10^-3)){
+  Recomp=f.Comput_GasEx_6800(LICOR6800_data = Aci,gcw = gcw)
+  Recomp_Aci_Panama=rbind.data.frame(Recomp_Aci_Panama,data.frame(A=Aci$A,
+                                                    Tleaf=Aci$Tleaf,
+                                                    Ci=Recomp$Ci,
+                                                    Qin=Aci$Qin,
+                                                    gsw=Recomp$gsw,
+                                                    Recomp=gcw))
+
+}
+
+Recomp_Aci_Panama$Species='Guatteria dumetorum'
+Recomp_Aci_Panama$mean_gbw=mean(2*Aci$gbw)
+
+#############################################################
+#### Recalculation of the ACi values from the Arctic data ###
+#############################################################
+Aci=Aci_data_arctic[order(Aci_data_arctic$Ci),]
+Recomp_Aci_arctic=Aci[,c('Photo','Tleaf','Ci','PARi','Cond')]
+Recomp_Aci_arctic$Recomp='Reference'
+
+## I recompute this file using Marquez et al. 2021 theory using different values 
+## of cuticular conductance
+for(gcw in seq(0,25*10^-3,1*10^-3)){
+  Recomp=f.Comput_GasEx_6400(LICOR6400_data = Aci,gcw = gcw)
+  Recomp_Aci_arctic=rbind.data.frame(Recomp_Aci_arctic,data.frame(Photo=Aci$Photo,
+                                                    Tleaf=Aci$Tleaf,
+                                                    Ci=Recomp$Ci,
+                                                    PARi=Aci$PARi,
+                                                    Cond=Recomp$gsw,
+                                                    Recomp=gcw))
+  
+}
+Recomp_Aci_arctic$Species='Petasites frigidus'
+Recomp_Aci_arctic$mean_gbw=mean(2*Aci_data_arctic$BLCond)
+colnames(Recomp_Aci_arctic)=colnames(Recomp_Aci_Panama)
+
+## Effect of different gcw on an Aci curve
+ggplot(data=Recomp_Aci_arctic[Recomp_Aci_arctic$Recomp!='Reference',],aes(x=Ci,y=A,color=Recomp))+geom_point()+xlab(expression(italic(C)[i]~calculated~using~Marquez~et~al.~2021))+labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+
+##########################################################
+#### Recalculation of the ACi values from the Oak data ###
+##########################################################
+
+Aci=Aci_data_oak[order(Aci_data_oak$Ci),]
+Recomp_Aci_oak=Aci[,c('Photo','Tleaf','Ci','PARi','Cond')]
+Recomp_Aci_oak$Recomp='Reference'
+
+## I recompute this file using Marquez et al. 2021 theory using different values 
+## of cuticular conductance
+for(gcw in seq(0,25*10^-3,1*10^-3)){
+  Recomp=f.Comput_GasEx_6400(LICOR6400_data = Aci,gcw = gcw)
+  Recomp_Aci_oak=rbind.data.frame(Recomp_Aci_oak,data.frame(Photo=Aci$Photo,
+                                                                  Tleaf=Aci$Tleaf,
+                                                                  Ci=Recomp$Ci,
+                                                                  PARi=Aci$PARi,
+                                                                  Cond=Recomp$gsw,
+                                                                  Recomp=gcw))
+  
+}
+Recomp_Aci_oak$Species='Quercus coccinea Münchh'
+Recomp_Aci_oak$mean_gbw=mean(2*Aci_data_oak$BLCond)
+colnames(Recomp_Aci_oak)=colnames(Recomp_Aci_Panama)
+
+Recomp_Aci=rbind.data.frame(Recomp_Aci_Panama,Recomp_Aci_arctic,Recomp_Aci_oak)
+
+#######################################
+## Figure effect of cc on Aci curves ##
+#######################################
+
+# First, I check if the A-Ci curves measured by the LICOR correspond to the recomputed values for gcw = 0
+
+plot(x=Recomp_Aci[Recomp_Aci$Recomp=='Reference','Ci'],y=Recomp_Aci[Recomp_Aci$Recomp=='0','Ci'])
+abline(c(0,1))
+       
+# Figure of the effect of cc
+data_fig=Recomp_Aci[Recomp_Aci$Recomp!='Reference',]
+data_fig$Recomp=as.numeric(data_fig$Recomp)  
+data_fig$Species=factor(as.character(data_fig$Species),levels = c("Quercus coccinea Münchh","Petasites frigidus","Guatteria dumetorum"),ordered = TRUE)
+library(viridis)
+jpeg(filename = 'Figure1.jpeg',width = 150,height = 92,units = 'mm',res=300)
+(ggplot(data=data_fig,
+       aes(x=Ci,y=A,color=Recomp,shape=Species))+geom_point()
+      +ylab(expression(italic(A)[n]~mu*mol~m^-2~s^-1))
+      +xlab(expression(italic(C)[i]~ppm))
+      +labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+  + scale_color_gradientn(colours = c('grey92','grey60','grey20'))
+      +scale_shape_discrete(labels=
+          c(expression(italic(Quercus~cocinear)~Münchh),
+            expression(italic(Petasides~frigidus)),
+            expression(italic(Guatteria~dumetorum))
+            ))
+      +theme_bw()
+  +theme(legend.text.align = 0,panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
+dev.off()
+
+###########################################################################
+## Effect of the differnt Aci curves on the estimation of the parameters ##
+## of the A-Ci curves                                                    ##
+###########################################################################
+
+## Correcting the units of the temperature
+Recomp_Aci_Panama$Tleaf=Recomp_Aci_Panama$Tleaf+273.16
+Recomp_Aci_arctic$Tleaf=Recomp_Aci_arctic$Tleaf+273.16
+Recomp_Aci_oak$Tleaf=Recomp_Aci_oak$Tleaf+273.16
+
+
+##### Panama
+
+Without_TPU=f.fitting(measures = Recomp_Aci_Panama[Recomp_Aci_Panama$Recomp=='Reference',],id.name = 'Species',
+                      Start =list(JmaxRef=65,RdRef=1,VcmaxRef=45),
+                      param = f.make.param(TpRef=100),type = 'Aci')
+
+With_TPU=f.fitting(measures = Recomp_Aci_Panama[Recomp_Aci_Panama$Recomp=='Reference',],id.name = 'Species',
+                   Start =list(JmaxRef=Without_TPU[[2]]@coef['JmaxRef'],
+                               RdRef=Without_TPU[[2]]@coef['RdRef'],
+                               VcmaxRef=Without_TPU[[2]]@coef['VcmaxRef'],
+                               TpRef=Without_TPU[[2]]@coef['VcmaxRef']/10),
+                   param = f.make.param(),type = 'Aci')
+
+AIC(Without_TPU[[2]])
+AIC(With_TPU[[2]])
+
+pdf(file = '1_Aci_fitting_Panama.pdf')
+result_various_gcw_Panama=by(data = Recomp_Aci_Panama,INDICES = list(Recomp_Aci_Panama$Recomp),
+                            FUN = function(x){
+                              f.fitting(measures = x,
+                                        Start = list(JmaxRef=With_TPU[[2]]@coef['JmaxRef'],
+                                                     RdRef=With_TPU[[2]]@coef['RdRef'],
+                                                     VcmaxRef=With_TPU[[2]]@coef['VcmaxRef'],
+                                                     TpRef=With_TPU[[2]]@coef['TpRef']),
+                                        param=f.make.param(),
+                                        id.name = 'Recomp',modify.init = FALSE)})
+dev.off()
+
+
+res_Panama=as.data.frame(t(sapply(result_various_gcw_Panama,
+                                             FUN = function(x) c(x[[2]]@coef,
+                                                                 AIC=AIC(x[[2]])))))
+res_Panama$Recomp=row.names(res_Panama)
+
+
+##### arctic
+
+Without_TPU=f.fitting(measures = Recomp_Aci_arctic[Recomp_Aci_arctic$Recomp=='Reference',],id.name = 'Species',
+                      Start =list(JmaxRef=65,RdRef=1,VcmaxRef=45),
+                      param = f.make.param(TpRef=100),type = 'Aci')
+
+With_TPU=f.fitting(measures = Recomp_Aci_arctic[Recomp_Aci_arctic$Recomp=='Reference',],id.name = 'Species',
+                   Start =list(JmaxRef=Without_TPU[[2]]@coef['JmaxRef'],
+                               RdRef=Without_TPU[[2]]@coef['RdRef'],
+                               VcmaxRef=Without_TPU[[2]]@coef['VcmaxRef'],
+                               TpRef=Without_TPU[[2]]@coef['VcmaxRef']/10),
+                   param = f.make.param(),type = 'Aci')
+
+AIC(Without_TPU[[2]])
+AIC(With_TPU[[2]])
+
+pdf(file = '1_Aci_fitting_arctic.pdf')
+result_various_gcw_arctic=by(data = Recomp_Aci_arctic,INDICES = list(Recomp_Aci_arctic$Recomp),
+                             FUN = function(x){
+                               f.fitting(measures = x,
+                                         Start = list(JmaxRef=Without_TPU[[2]]@coef['JmaxRef'],
+                                                      RdRef=Without_TPU[[2]]@coef['RdRef'],
+                                                      VcmaxRef=Without_TPU[[2]]@coef['VcmaxRef']),
+                                         param=f.make.param(TpRef = 100),
+                                         id.name = 'Recomp',modify.init = FALSE)})
+dev.off()
+
+
+res_arctic=as.data.frame(t(sapply(result_various_gcw_arctic,
+                                  FUN = function(x) c(x[[2]]@coef,
+                                                      AIC=AIC(x[[2]])))))
+res_arctic$Recomp=row.names(res_arctic)
+
+
+######## oak 
+
+
+##### oak
+
+Without_TPU=f.fitting(measures = Recomp_Aci_oak[Recomp_Aci_oak$Recomp=='Reference',],id.name = 'Species',
+                      Start =list(JmaxRef=65,RdRef=1,VcmaxRef=45),
+                      param = f.make.param(TpRef=100),type = 'Aci')
+
+With_TPU=f.fitting(measures = Recomp_Aci_oak[Recomp_Aci_oak$Recomp=='Reference',],id.name = 'Species',
+                   Start =list(JmaxRef=Without_TPU[[2]]@coef['JmaxRef'],
+                               RdRef=Without_TPU[[2]]@coef['RdRef'],
+                               VcmaxRef=Without_TPU[[2]]@coef['VcmaxRef'],
+                               TpRef=Without_TPU[[2]]@coef['VcmaxRef']/10),
+                   param = f.make.param(),type = 'Aci')
+
+AIC(Without_TPU[[2]])
+AIC(With_TPU[[2]])
+
+pdf(file = '1_Aci_fitting_oak.pdf')
+result_various_gcw_oak=by(data = Recomp_Aci_oak,INDICES = list(Recomp_Aci_oak$Recomp),
+                             FUN = function(x){
+                               f.fitting(measures = x,
+                                         Start = list(JmaxRef=Without_TPU[[2]]@coef['JmaxRef'],
+                                                      RdRef=Without_TPU[[2]]@coef['RdRef'],
+                                                      VcmaxRef=Without_TPU[[2]]@coef['VcmaxRef']),
+                                         param=f.make.param(TpRef = 100),
+                                         id.name = 'Recomp',modify.init = FALSE)})
+dev.off()
+
+
+res_oak=as.data.frame(t(sapply(result_various_gcw_oak,
+                                  FUN = function(x) c(x[[2]]@coef,
+                                                      AIC=AIC(x[[2]])))))
+res_oak$Recomp=row.names(res_oak)
+
+
+
+#########################
+### Figure parameters ###
+#########################
+res_oak$TpRef=NA
+res_arctic$TpRef=NA
+res_oak$Species="Quercus coccinea Münchh"
+res_Panama$Species="Guatteria dumetorum"
+res_arctic$Species="Petasites frigidus"
+res_all=rbind.data.frame(res_oak[,colnames(res_Panama)],res_arctic[,colnames(res_Panama)],res_Panama)
+res_all$Species=factor(as.character(res_all$Species),levels = c("Quercus coccinea Münchh","Petasites frigidus","Guatteria dumetorum"),ordered = TRUE)
+
+data_fig=res_all[res_all$Recomp!='Reference',]
+for(species in unique(data_fig$Species)){
+  data_fig[data_fig$Species==species,'Vcmax25']=data_fig[data_fig$Species==species,'VcmaxRef']/data_fig[data_fig$Species==species&data_fig$Recomp==0,'VcmaxRef']
+  data_fig[data_fig$Species==species,'Jmax25']=data_fig[data_fig$Species==species,'JmaxRef']/data_fig[data_fig$Species==species&data_fig$Recomp==0,'JmaxRef']
+  data_fig[data_fig$Species==species,'Tp25']=data_fig[data_fig$Species==species,'TpRef']/data_fig[data_fig$Species==species&data_fig$Recomp==0,'TpRef']
+  data_fig[data_fig$Species==species,'Rd25']=data_fig[data_fig$Species==species,'RdRef']/data_fig[data_fig$Species==species&data_fig$Recomp==0,'RdRef']
+ }
+data_fig$Recomp=as.numeric(data_fig$Recomp) 
+size_p=1
+a=(ggplot(data=data_fig,
+          aes(x=Recomp,y=Vcmax25,shape=Species))+geom_point(size=size_p)
+   +ylab(expression(Standardized~italic(V)[cmax]))
+   +xlab(expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +ylim(c(0.5,2))
+   #+ scale_color_gradientn(colours = c('grey92','grey60','grey20'))
+   +scale_shape_discrete(labels=
+                           c(expression(italic(Quercus~cocinear)~Münchh),
+                             expression(italic(Petasides~frigidus)),
+                             expression(italic(Guatteria~dumetorum))
+                           ))
+   +theme_bw()
+   +theme(legend.text.align = 0,panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
+
+
+b=(ggplot(data=data_fig,
+          aes(x=Recomp,y=Jmax25,shape=Species))+geom_point(size=size_p)
+   +ylab(expression(Standardized~italic(J)[max]))
+   +xlab(expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +ylim(c(0.5,2))
+   #+ scale_color_gradientn(colours = c('grey92','grey60','grey20'))
+   +scale_shape_discrete(labels=
+                           c(expression(italic(Quercus~cocinear)~Münchh),
+                             expression(italic(Petasides~frigidus)),
+                             expression(italic(Guatteria~dumetorum))
+                           ))
+   +theme_bw()
+   +theme(legend.text.align = 0,panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.position='none'))
+
+
+
+c=(ggplot(data=data_fig,
+          aes(x=Recomp,y=Tp25,shape=Species))+geom_point(size=size_p)
+   +ylab(expression(Standardized~italic(TPU)))
+   +xlab(expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +ylim(c(0.5,2))
+   #+ scale_color_gradientn(colours = c('grey92','grey60','grey20'))
+   +scale_shape_discrete(labels=
+                           c(expression(italic(Quercus~cocinear)~Münchh),
+                             expression(italic(Petasides~frigidus)),
+                             expression(italic(Guatteria~dumetorum))
+                           ))
+   +theme_bw()
+   +theme(legend.text.align = 0,panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.position='none'))
+
+
+
+
+d=(ggplot(data=data_fig,
+          aes(x=Recomp,y=Rd25,shape=Species))+geom_point(size=size_p)
+   +ylab(expression(Standardized~italic(R)[day]))
+   +xlab(expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +labs(color = expression(italic(g)[cw]~mol~m^-2~s^-1))
+   +ylim(c(0.5,2))
+   #+ scale_color_gradientn(colours = c('grey92','grey60','grey20'))
+   +scale_shape_discrete(labels=
+                           c(expression(italic(Quercus~cocinear)~Münchh),
+                             expression(italic(Petasides~frigidus)),
+                             expression(italic(Guatteria~dumetorum))
+                           ))
+   +theme_bw()
+   +theme(legend.text.align = 0,panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.position='none'))
+
+jpeg(filename = 'Figure2.jpeg',width = 210,height = 145,units = 'mm',res=300)
+half=plot_grid(a+theme(legend.position = 'none'),b,c,d,align='hv',ncol=2,labels = 'auto')
+leg=get_legend(a)
+plot_grid(half,leg,ncol = 2,rel_widths = c(0.75,0.25))
+dev.off()
+
